53e2ab3d6dd4e2f478e25651f90259c95dbc6e01
matiasbrascescoglobant <matias.brascesco@globant.com>
2021-12-14 16:19:42 -0300

    Initial commit. gitignore.

---
+++ src/main/java/mnbd/jbm/spring/jbm/JbmApplication.java
@@ -0,0 +1,13 @@
+package mnbd.jbm.spring.jbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mnbd/jbm/spring/jbm/JbmApplicationTests.java
@@ -0,0 +1,13 @@
+package mnbd.jbm.spring.jbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
